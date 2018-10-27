64b88bba4fa18b4119995f30a485578e13597cbf
johnnybee4e <jon.rosado42@gmail.com>
2018-10-26 21:28:07 -0400

    hard code a query parameter to get a response from the GIPHY search api endpoint.

--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+require('dotenv').config();
+const GphApiClient = require('giphy-js-sdk-core');
+const client = GphApiClient(process.env.GIPHY_API_KEY);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/tag', (req, res, next) => {
-  res.send("You're it!");
+app.get('/tag', async (req, res, next) => {
+    try {
+        let gifRes = await client.search('gifs', {'q': 'tag'});
+        res.json(gifRes.data);
+    } catch(err) {
+        console.error(err);
+    }
 });
 
 app.use((err, req, res, next) => {
