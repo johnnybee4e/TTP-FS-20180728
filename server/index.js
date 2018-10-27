19c6f1ac98897e988ec0058fa14fe8080743dd48
johnnybee4e <jon.rosado42@gmail.com>
2018-10-26 22:49:46 -0400

    remove deprecated giphy search endpoint from server/index.js

--- server/index.js
+++ server/index.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-// require('dotenv').config();
-// const GphApiClient = require('giphy-js-sdk-core');
-// const client = GphApiClient(process.env.GIPHY_API_KEY);
+
 
 if (process.env.NODE_ENV !== 'production') require('../secrets')
 
@@ -13,14 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', require('./api'));
-// app.get('/api/trending', async (req, res, next) => {
-//     try {
-//         let gifRes = await client.trending('gifs', {});
-//         res.json(gifRes.data);
-//     } catch(err) {
-//         console.error(err);
-//     }
-// });
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
