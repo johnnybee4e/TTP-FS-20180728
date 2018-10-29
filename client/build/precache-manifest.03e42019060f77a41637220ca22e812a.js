226778a8d2212006aedcc1f6c2aac1f91aec4088
johnnybee4e <jon.rosado42@gmail.com>
2018-10-28 23:57:30 -0400

    rebuilding app for css changes to take effect

---
+++ client/build/precache-manifest.03e42019060f77a41637220ca22e812a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6f6b9a71ab1135c19c8",
+    "url": "/static/js/main.b6f6b9a7.chunk.js"
+  },
+  {
+    "revision": "9acebd241f61cfbd2f4b",
+    "url": "/static/js/1.9acebd24.chunk.js"
+  },
+  {
+    "revision": "b6f6b9a71ab1135c19c8",
+    "url": "/static/css/main.7b2ac112.chunk.css"
+  },
+  {
+    "revision": "e8a5dd77dc183c0b03b128b9241b508e",
+    "url": "/index.html"
+  }
+];
