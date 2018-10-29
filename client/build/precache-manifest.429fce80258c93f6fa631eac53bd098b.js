b8baead44e03e3b8905402332a7e842ccf1ac333
johnnybee4e <jon.rosado42@gmail.com>
2018-10-28 22:25:42 -0400

    adding build to source control

---
+++ client/build/precache-manifest.429fce80258c93f6fa631eac53bd098b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a32cc51e3078225fa76",
+    "url": "/static/js/main.1a32cc51.chunk.js"
+  },
+  {
+    "revision": "c4248fc9d46b901916ea",
+    "url": "/static/js/1.c4248fc9.chunk.js"
+  },
+  {
+    "revision": "1a32cc51e3078225fa76",
+    "url": "/static/css/main.ef3c6647.chunk.css"
+  },
+  {
+    "revision": "4fd4a2e09cfd76fbfb939cee7c5d5506",
+    "url": "/index.html"
+  }
+];
